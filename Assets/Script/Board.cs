ed598617d3eaf864f616bb689d87c92c6298a993
Thomien Leygonie <124178882+ToddyTreaks@users.noreply.github.com>
2024-05-21 18:27:14 +0200

    Les pièces devrait marcher avec verifiaction des mouvement reste les Roc && la prise ne passant && la promotion des pionts

--- Assets/Script/Board.cs
+++ Assets/Script/Board.cs
@@ -1,19 +1,16 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    public Piece[,] board = new Piece[8, 8];
-    private static Board instance;
+    #region variables
+    
+    public static Piece[,] board = new Piece[8, 8];
+    
+    #endregion
 
-    private void Awake()
-    {
-        instance = this;
-    }
-
+    #region Initialisation
+    
     private void Start()
     {
         for (int i = 2; i < 6; i++)
@@ -23,42 +20,267 @@ public class Board : MonoBehaviour
                 board[i, j] = null;
             }
         }
-        
+
         for (int i = 0; i < 8; i++)
         {
             AddPiece(new Pawn(new Position(1, i), Team.WHITE), 1, i);
             AddPiece(new Pawn(new Position(6, i), Team.BLACK), 6, i);
         }
-        
-        AddPiece(new Rock(new Position(0,0), Team.WHITE), 0, 0);
+
+        AddPiece(new Rock(new Position(0, 0), Team.WHITE), 0, 0);
         AddPiece(new Rock(new Position(0, 7), Team.WHITE), 0, 7);
         AddPiece(new Rock(new Position(7, 0), Team.BLACK), 7, 0);
         AddPiece(new Rock(new Position(7, 7), Team.BLACK), 7, 7);
-        
+
         AddPiece(new Knight(new Position(0, 1), Team.WHITE), 0, 1);
         AddPiece(new Knight(new Position(0, 6), Team.WHITE), 0, 6);
         AddPiece(new Knight(new Position(7, 1), Team.BLACK), 7, 1);
         AddPiece(new Knight(new Position(7, 6), Team.BLACK), 7, 6);
-        
+
         AddPiece(new Bishop(new Position(0, 2), Team.WHITE), 0, 2);
         AddPiece(new Bishop(new Position(0, 5), Team.WHITE), 0, 5);
         AddPiece(new Bishop(new Position(7, 2), Team.BLACK), 7, 2);
         AddPiece(new Bishop(new Position(7, 5), Team.BLACK), 7, 5);
-        
+
         AddPiece(new Queen(new Position(0, 3), Team.WHITE), 0, 3);
         AddPiece(new Queen(new Position(7, 3), Team.BLACK), 7, 3);
-        
+
         AddPiece(new King(new Position(0, 4), Team.WHITE), 0, 4);
         AddPiece(new King(new Position(7, 4), Team.BLACK), 7, 4);
     }
+    
+    #endregion
 
-    public void AddPiece(Piece piece, int xIndex, int yIndex)
+    #region Modification Board
+    public static void AddPiece(Piece piece, int xIndex, int yIndex)
     {
         board[xIndex, yIndex] = piece;
     }
 
-    public void RemovePiece(int xIndex, int yIndex)
+    public static void RemovePiece(int xIndex, int yIndex)
     {
         board[xIndex, yIndex] = null;
     }
+    
+    #endregion
+    
+    #region CheckMoveDirection
+    
+    public static int CheckMoveUp(Piece inputPiece, int maxDistance = 7)
+    {
+        int x = inputPiece.actualPosition.xIndex;
+        int y = inputPiece.actualPosition.yIndex;
+
+        for (int i = 1; i <= maxDistance; i++)
+        {
+            if (y + i > 7)
+            {
+                return i - 1; // Cas hors plateau
+            }
+
+            if (board[x, y + i] != null)
+            {
+                if (board[x, y + i].type == PieceType.PAWN)
+                {
+                    return i - 1;
+                }
+                else if (board[x, y + i].team != inputPiece.team)
+                {
+                    return i;
+                }
+
+                return i - 1;
+            }
+        }
+
+        throw new Exception("CheckMoveUp : déplacement impossible");
+    }
+
+    public static int CheckMoveDown(Piece inputPiece, int maxDistance = 7)
+    {
+        int x = inputPiece.actualPosition.xIndex;
+        int y = inputPiece.actualPosition.yIndex;
+
+        for (int i = 1; i <= maxDistance; i++)
+        {
+            if (y - i < 0)
+            {
+                return i - 1;
+            }
+
+            if (board[x, y - i] != null)
+            {
+                if (board[x, y - i].type == PieceType.PAWN)
+                {
+                    return i - 1;
+                }
+                else if (board[x, y - i].team != inputPiece.team)
+                {
+                    return i;
+                }
+
+                return i - 1;
+            }
+        }
+
+        throw new Exception("CheckMoveDown : déplacement impossible");
+    }
+
+    public static int CheckMoveRight(Piece inputPiece, int maxDistance = 7)
+    {
+        int x = inputPiece.actualPosition.xIndex;
+        int y = inputPiece.actualPosition.yIndex;
+
+        for (int i = 1; i <= maxDistance; i++)
+        {
+            if (x + i > 7)
+            {
+                return i - 1;
+            }
+
+            if (board[x + i, y] != null)
+            {
+                if (board[x + i, y].team != inputPiece.team)
+                {
+                    return i;
+                }
+
+                return i - 1;
+            }
+        }
+
+        throw new Exception("CheckMoveRight : déplacement impossible");
+    }
+
+    public static int CheckMoveLeft(Piece inputPiece, int maxDistance = 7)
+    {
+        int x = inputPiece.actualPosition.xIndex;
+        int y = inputPiece.actualPosition.yIndex;
+
+        for (int i = 1; i <= maxDistance; i++)
+        {
+            if (x - i < 0)
+            {
+                return i - 1;
+            }
+
+            if (board[x - i, y] != null)
+            {
+                if (board[x - i, y].team != inputPiece.team)
+                {
+                    return i;
+                }
+
+                return i - 1;
+            }
+        }
+
+        throw new Exception("CheckMoveLeft : déplacement impossible");
+    }
+
+    public static int CheckMoveUpRight(Piece inputPiece, int maxDistance = 7)
+    {
+        int x = inputPiece.actualPosition.xIndex;
+        int y = inputPiece.actualPosition.yIndex;
+
+        for (int i = 1; i <= maxDistance; i++)
+        {
+            if (x + i > 7 || y + i > 7)
+            {
+                return i - 1;
+            }
+
+            if (board[x + i, y + i] != null)
+            {
+                if (board[x + i, y + i].team != inputPiece.team)
+                {
+                    return i;
+                }
+
+                return i - 1;
+            }
+        }
+
+        throw new Exception("CheckMoveUpRight : déplacement impossible");
+    }
+
+    public static int CheckMoveUpLeft(Piece inputPiece, int maxDistance = 7)
+    {
+        int x = inputPiece.actualPosition.xIndex;
+        int y = inputPiece.actualPosition.yIndex;
+
+        for (int i = 1; i <= maxDistance; i++)
+        {
+            if (x - i < 0 || y + i > 7)
+            {
+                return i - 1;
+            }
+
+            if (board[x - i, y + i] != null)
+            {
+                if (board[x - i, y + i].team != inputPiece.team)
+                {
+                    return i;
+                }
+
+                return i - 1;
+            }
+        }
+
+        throw new Exception("CheckMoveUpLeft : déplacement impossible");
+    }
+
+    public static int CheckMoveDownRight(Piece inputPiece, int maxDistance = 7)
+    {
+        int x = inputPiece.actualPosition.xIndex;
+        int y = inputPiece.actualPosition.yIndex;
+
+        for (int i = 1; i <= maxDistance; i++)
+        {
+            if (x + i > 7 || y - i < 0)
+            {
+                return i - 1;
+            }
+
+            if (board[x + i, y - i] != null)
+            {
+                if (board[x + i, y - i].team != inputPiece.team)
+                {
+                    return i;
+                }
+
+                return i - 1;
+            }
+        }
+
+        throw new Exception("CheckMoveDownRight : déplacement impossible");
+    }
+
+    public static int CheckMoveDownLeft(Piece inputPiece, int maxDistance = 7)
+    {
+        int x = inputPiece.actualPosition.xIndex;
+        int y = inputPiece.actualPosition.yIndex;
+
+        for (int i = 1; i <= maxDistance; i++)
+        {
+            if (x - i < 0 || y - i < 0)
+            {
+                return i - 1;
+            }
+
+            if (board[x - i, y - i] != null)
+            {
+                if (board[x - i, y - i].team != inputPiece.team)
+                {
+                    return i;
+                }
+
+                return i - 1;
+            }
+        }
+
+        throw new Exception("CheckMoveDownLeft : déplacement impossible");
+    }
+    
+    #endregion
 }
--- Assets/Script/Piece.cs
+++ Assets/Script/Piece.cs
@@ -1,7 +1,8 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+#region Enums PiceType and Team
 
 public enum PieceType
 {
@@ -19,6 +20,10 @@ public enum Team
     BLACK
 }
 
+#endregion
+
+#region Class Position
+
 public class Position
 {
     public int xIndex;
@@ -30,6 +35,10 @@ public class Position
         this.yIndex = yIndex;
     }
 }
+
+#endregion
+
+#region MotherClass Piece
 
 public abstract class Piece
 {
@@ -44,71 +53,361 @@ public abstract class Piece
         this.team = team;
     }
 
-    public void Move(Position targetPosition)
+    public List<Position> SelectedPiece()
     {
-        IsPositionInBoard(targetPosition);
-
-        CanGoTo(targetPosition);
+        return MoveAllowed();
     }
 
-    protected virtual bool CanGoTo(Position targetPosition)
+    protected virtual List<Position> MoveAllowed()
     {
-        return false;
+        return new List<Position>();
     }
 
-    private void IsPositionInBoard(Position targetPosition)
+    public void MovePiece(Piece piece, Position targetPosition)
     {
-        if (targetPosition.xIndex < 0 || targetPosition.xIndex > 7)
-        {
-            throw new Exception("Invalid x index");
-        }
-
-        if (targetPosition.yIndex < 0 || targetPosition.yIndex > 7)
-        {
-            throw new Exception("Invalid y index");
-        }
+        /*Attention si ça pète c'est ici*/
+        Board.AddPiece(piece, targetPosition.xIndex, targetPosition.yIndex);
+        Board.RemovePiece(piece.actualPosition.xIndex, piece.actualPosition.yIndex);
+        Debug.Log("Move Piece : Pièce déplacé & ça ne pète pas");
     }
 }
 
+#endregion
+
+#region Pawn
 
 public class Pawn : Piece
 {
+    private bool firstMove = true;
+    private bool canTake = false;
+
     public Pawn(Position position, Team team) : base(position, PieceType.PAWN, team)
     {
     }
+
+    protected override List<Position> MoveAllowed()
+    {
+        List<Position> targetablePos = new List<Position>();
+
+        if (team == Team.WHITE)
+        {
+            if (firstMove)
+            {
+                int distance = Board.CheckMoveUp(this, 2);
+                for (int i = 1; i < distance; i++)
+                {
+                    targetablePos.Add(new Position(this.actualPosition.xIndex + i, this.actualPosition.yIndex));
+                }
+
+                firstMove = false;
+            }
+            else
+            {
+                int distance = Board.CheckMoveUp(this, 1);
+                if (distance == 1)
+                {
+                    targetablePos.Add(new Position(this.actualPosition.xIndex + 1, this.actualPosition.yIndex));
+                }
+            }
+        }
+        else
+        {
+            if (firstMove)
+            {
+                int distance = Board.CheckMoveDown(this, 2);
+                for (int i = 1; i < distance; i++)
+                {
+                    targetablePos.Add(new Position(this.actualPosition.xIndex - i, this.actualPosition.yIndex));
+                }
+
+                firstMove = false;
+            }
+            else
+            {
+                int distance = Board.CheckMoveDown(this, 1);
+                if (distance == 1)
+                {
+                    targetablePos.Add(new Position(this.actualPosition.xIndex - 1, this.actualPosition.yIndex));
+                }
+            }
+        }
+
+        return targetablePos;
+    }
 }
+
+#endregion
+
+#region Rock
 
 public class Rock : Piece
 {
+    private bool firstMove = true;
+
     public Rock(Position position, Team team) : base(position, PieceType.ROCK, team)
     {
     }
+
+    protected override List<Position> MoveAllowed()
+    {
+        List<Position> targetablePos = new List<Position>();
+
+        int distanceUp = Board.CheckMoveUp(this);
+        int distanceDown = Board.CheckMoveDown(this);
+        int distanceLeft = Board.CheckMoveLeft(this);
+        int distanceRight = Board.CheckMoveRight(this);
+
+        for (int i = 1; i < distanceUp; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex + i, this.actualPosition.yIndex));
+        }
+
+        for (int i = 1; i < distanceDown; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex - i, this.actualPosition.yIndex));
+        }
+
+        for (int i = 1; i < distanceLeft; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex, this.actualPosition.yIndex - i));
+        }
+
+        for (int i = 1; i < distanceRight; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex, this.actualPosition.yIndex + i));
+        }
+
+        return targetablePos;
+    }
 }
+
+#endregion
+
+#region Knight
 
 public class Knight : Piece
 {
     public Knight(Position position, Team team) : base(position, PieceType.KNIGHT, team)
     {
     }
+
+    protected override List<Position> MoveAllowed()
+    {
+        List<Position> targetablePos = new List<Position>();
+
+        List<Position> knightPos = KnightPos();
+
+        foreach (var pos in knightPos)
+        {
+            if (Board.board[pos.xIndex, pos.yIndex] == null || Board.board[pos.xIndex, pos.yIndex].team != this.team)
+                targetablePos.Add(pos);
+        }
+
+        return targetablePos;
+    }
+
+    private List<Position> KnightPos()
+    {
+        List<Position> knightPos = new List<Position>();
+
+        if (this.actualPosition.xIndex + 2 < 8 && this.actualPosition.yIndex + 1 < 8)
+            knightPos.Add(new Position(this.actualPosition.xIndex + 2, this.actualPosition.yIndex + 1));
+        if (this.actualPosition.xIndex + 2 < 8 && this.actualPosition.yIndex - 1 >= 0)
+            knightPos.Add(new Position(this.actualPosition.xIndex + 2, this.actualPosition.yIndex - 1));
+        if (this.actualPosition.xIndex - 2 >= 0 && this.actualPosition.yIndex + 1 < 8)
+            knightPos.Add(new Position(this.actualPosition.xIndex - 2, this.actualPosition.yIndex + 1));
+        if (this.actualPosition.xIndex - 2 >= 0 && this.actualPosition.yIndex - 1 >= 0)
+            knightPos.Add(new Position(this.actualPosition.xIndex - 2, this.actualPosition.yIndex - 1));
+        if (this.actualPosition.xIndex + 1 < 8 && this.actualPosition.yIndex + 2 < 8)
+            knightPos.Add(new Position(this.actualPosition.xIndex + 1, this.actualPosition.yIndex + 2));
+        if (this.actualPosition.xIndex + 1 < 8 && this.actualPosition.yIndex - 2 >= 0)
+            knightPos.Add(new Position(this.actualPosition.xIndex + 1, this.actualPosition.yIndex - 2));
+        if (this.actualPosition.xIndex - 1 >= 0 && this.actualPosition.yIndex + 2 < 8)
+            knightPos.Add(new Position(this.actualPosition.xIndex - 1, this.actualPosition.yIndex + 2));
+        if (this.actualPosition.xIndex - 1 >= 0 && this.actualPosition.yIndex - 2 >= 0)
+            knightPos.Add(new Position(this.actualPosition.xIndex - 1, this.actualPosition.yIndex - 2));
+
+        return KnightPos();
+    }
 }
+
+#endregion
+
+#region Bishop
 
 public class Bishop : Piece
 {
     public Bishop(Position position, Team team) : base(position, PieceType.BISHOP, team)
     {
     }
+
+    protected override List<Position> MoveAllowed()
+    {
+        List<Position> targetablePos = new List<Position>();
+
+        int distanceUpLeft = Board.CheckMoveUpLeft(this);
+        int distanceUpRight = Board.CheckMoveUpRight(this);
+        int distanceDownLeft = Board.CheckMoveDownLeft(this);
+        int distanceDownRight = Board.CheckMoveDownRight(this);
+
+        for (int i = 1; i < distanceUpLeft; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex + i, this.actualPosition.yIndex - i));
+        }
+
+        for (int i = 1; i < distanceUpRight; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex + i, this.actualPosition.yIndex + i));
+        }
+
+        for (int i = 1; i < distanceDownLeft; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex - i, this.actualPosition.yIndex - i));
+        }
+
+        for (int i = 1; i < distanceDownRight; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex - i, this.actualPosition.yIndex + i));
+        }
+
+        return targetablePos;
+    }
 }
+
+#endregion
+
+#region Queen
 
 public class Queen : Piece
 {
     public Queen(Position position, Team team) : base(position, PieceType.QUEEN, team)
     {
     }
+
+    protected override List<Position> MoveAllowed()
+    {
+        List<Position> targetablePos = new List<Position>();
+
+        int distanceUp = Board.CheckMoveUp(this);
+        int distanceDown = Board.CheckMoveDown(this);
+        int distanceLeft = Board.CheckMoveLeft(this);
+        int distanceRight = Board.CheckMoveRight(this);
+        int distanceUpLeft = Board.CheckMoveUpLeft(this);
+        int distanceUpRight = Board.CheckMoveUpRight(this);
+        int distanceDownLeft = Board.CheckMoveDownLeft(this);
+        int distanceDownRight = Board.CheckMoveDownRight(this);
+
+        for (int i = 1; i < distanceUp; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex + i, this.actualPosition.yIndex));
+        }
+
+        for (int i = 1; i < distanceDown; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex - i, this.actualPosition.yIndex));
+        }
+
+        for (int i = 1; i < distanceLeft; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex, this.actualPosition.yIndex - i));
+        }
+
+        for (int i = 1; i < distanceRight; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex, this.actualPosition.yIndex + i));
+        }
+
+        for (int i = 1; i < distanceUpLeft; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex + i, this.actualPosition.yIndex - i));
+        }
+
+        for (int i = 1; i < distanceUpRight; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex + i, this.actualPosition.yIndex + i));
+        }
+
+        for (int i = 1; i < distanceDownLeft; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex - i, this.actualPosition.yIndex - i));
+        }
+
+        for (int i = 1; i < distanceDownRight; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex - i, this.actualPosition.yIndex + i));
+        }
+
+        return targetablePos;
+    }
 }
+
+#endregion
+
+#region King
 
 public class King : Piece
 {
+    private bool firstMove = true;
+
     public King(Position position, Team team) : base(position, PieceType.KING, team)
     {
     }
+
+    protected override List<Position> MoveAllowed()
+    {
+        List<Position> targetablePos = new List<Position>();
+
+        int distanceUp = Board.CheckMoveUp(this, 1);
+        int distanceDown = Board.CheckMoveDown(this, 1);
+        int distanceLeft = Board.CheckMoveLeft(this, 1);
+        int distanceRight = Board.CheckMoveRight(this, 1);
+        int distanceUpLeft = Board.CheckMoveUpLeft(this, 1);
+        int distanceUpRight = Board.CheckMoveUpRight(this, 1);
+        int distanceDownLeft = Board.CheckMoveDownLeft(this, 1);
+        int distanceDownRight = Board.CheckMoveDownRight(this, 1);
+
+        for (int i = 1; i < distanceUp; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex + i, this.actualPosition.yIndex));
+        }
+
+        for (int i = 1; i < distanceDown; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex - i, this.actualPosition.yIndex));
+        }
+
+        for (int i = 1; i < distanceLeft; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex, this.actualPosition.yIndex - i));
+        }
+
+        for (int i = 1; i < distanceRight; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex, this.actualPosition.yIndex + i));
+        }
+
+        for (int i = 1; i < distanceUpLeft; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex + i, this.actualPosition.yIndex - i));
+        }
+
+        for (int i = 1; i < distanceUpRight; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex + i, this.actualPosition.yIndex + i));
+        }
+
+        for (int i = 1; i < distanceDownLeft; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex - i, this.actualPosition.yIndex - i));
+        }
+
+        for (int i = 1; i < distanceDownRight; i++)
+        {
+            targetablePos.Add(new Position(this.actualPosition.xIndex - i, this.actualPosition.yIndex + i));
+        }
+
+        return targetablePos;
+    }
 }
+
+#endregion
